4c3c188e6832939d55e9f3ffbdde924e7e9679da
VENKATA SUBBAIAH PERUGU <108419537+Subbuperugu26@users.noreply.github.com>
2026-08-17 16:04:06 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Route path="/" element={<Home />} />
+    <BrowserRouter basename="/Trai-Track">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
